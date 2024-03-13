ccd040036a8d78172a340b26acce8e86d66a85af
williamenrique <william21enrique@gmail.com>
2024-03-12 20:20:18 -0400

    funcion

--- src/js/function.unidad.js
+++ src/js/function.unidad.js
@@ -20,6 +20,25 @@ function fntViewUnidad() {
 				var objData = JSON.parse(request.responseText);
                 if (objData.status) {
                     Swal.fire('info', objData.msg)
+					// 	document.querySelector("#unidad").innerHTML = objData.data.id_unidad
+				// 	document.querySelector("#unidad").innerHTML = objData.data.id_unidad
+				// 	document.querySelector("#vim").innerHTML = objData.data.vim_unidad
+				// 	document.querySelector("#modelo").innerHTML = objData.data.modelo_unidad
+				// 	document.querySelector("#txtRutaUnidad").value = objData.data.ruta_unidad
+				// 	document.querySelector("#txtFechaEntrada").value = objData.data.fecha_entrada
+				// 	document.querySelector("#txtOperador").value = objData.data.operardor_unidad
+				// 	document.querySelector("#txtMecanico").value = objData.data.nomb_mecanico
+				// 	document.querySelector("#txtCombustible").value = objData.data.tipo_combustible
+				// 	document.querySelector("#txtKM").value = objData.data.km_unidad
+				// 	document.querySelector("#txtDiagnostico").value = objData.data.diagnostico
+				// 	document.querySelector("#txtRecomendacion").value = objData.data.recomendacion
+				// 	document.querySelector("#txtCapacidad").value = objData.data.cap_pasajero
+				// 	document.querySelector("#txtCreacion").value = objData.data.fecha_creacion
+				// 	if(objData.data.tipo_mantenimiento == "c"){
+				// 		document.querySelector("#tipoMant").innerHTML = '<span class="badge badge-info">CORRECTIVO</span>'
+				// 	}else{
+				// 		document.querySelector("#tipoMant").innerHTML = '<span class="badge badge-info">PREVENTIVO</span>'
+				// 	}
                 } else {
                     Swal.fire('error', objData.msg)
                 }
--- system/app/Views/Flota/unidad.php
+++ system/app/Views/Flota/unidad.php
@@ -29,12 +29,65 @@
 					<div class="card">
 						<div class="card-header">
 							<h3 class="card-title">Informacion de la unidad</h3>
+                            <h3 class="card-title accent-light" > <span id="unidad"></span></h3>
+                            <h3 class="card-title accent-light" > <span id="marca"></span> - </h3>
+                            <h3 class="card-title" > <span id="modelo"></span> - </h3>
+                            <h3 class="card-title" > <span id="vim"></span></h3>
 						</div>
 
-						<div class="card-body">
-							Toda la informacion correspondiente a la unidad solicitada
-                            <input type="text" value="<?= $_GET['unidad']?>" id="idGetUnidad">
-						</div>
+                        <input type="hidden" value="<?= $_GET['unidad']?>" id="idGetUnidad">
+                        <div class="card-body">
+                            <form id="formUndMant">
+                                <div class="form-row align-items-center">
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">ENTRADA</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtFechaEntrada" name="txtFechaEntrada">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">OPERADOR</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtOperador" name="txtOperador">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">MECANICO</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtMecanico" name="txtMecanico">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">Combustible</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtCombustible" name="txtCombustible">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">KM</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtKM" name="txtKM">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">RUTA</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtRutaUnidad" name="txtRutaUnidad">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">CAPACIDAD</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtCapacidad" name="txtCapacidad">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName">AÑO</label>
+                                        <input type="text" class="form-control" disabled placeholder="" id="txtCreacion" name="txtCreacion">
+                                    </div>
+                                    <div class="col-sm-2 my-1">
+                                        <label class="" for="inlineFormInputName"></label>
+                                        <h4 id="tipoMant" class="my-2"></h5>
+                                    </div>
+                                    <div class="row">
+                                    <div class="col-sm-6 my-1">
+                                        <label class="" for="floatingTextarea2">Diagnostico</label>
+                                        <textarea class="form-control" disabled placeholder="Diagnostico" style="height: 50%" id="txtDiagnostico" name="txtDiagnostico"></textarea>
+                                    </div>
+                                    <div class="col-sm-6 my-1">
+                                        <label class="" for="floatingTextarea2">Recomendacion</label>
+                                        <textarea class="form-control" disabled placeholder="Recomendacion" style="height: 50%" id="txtRecomendacion" name="txtRecomendacion"></textarea>
+                                    </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
 						<!-- /.card-body -->
 						<!-- <div class="card-footer">
 							Footer
